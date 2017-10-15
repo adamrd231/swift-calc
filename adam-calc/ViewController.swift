742aeff621167ad57367643d80f25756c26df110
Adam Reed <adam.rdconcepts@gmail.com>
2017-10-14 22:20:51 -0700

    implement decimal feature, add two variable buttons, reformat

--- adam-calc/ViewController.swift
+++ adam-calc/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 import GoogleMobileAds
 
+
+// MARK: Global Variables
 // Variable for the current string of information on the main screen
 var currentDisplay:String = ""
 var operationIsDisplayed: Bool = false
 var operatorType:String?
 var firstNumber:Double?
 var secondNumber:Double?
-var answer:String = ""
+var answer:Double?
+var string_answer:String = ""
 var typeDoubleAnswer:Double?
-var variableIsLocked: Bool = false
+var variable1IsLocked: Bool = false
+var variable2IsLocked: Bool = false
+var variable3IsLocked: Bool = false
+var containsDecimal: Bool = false
 
 
 
@@ -25,21 +31,41 @@ class ViewController: UIViewController {
     
     //Variables from the UI saved into my ViewController Class to be used in button functionality etc.
     @IBOutlet weak var mainDisplay: UILabel!
-    @IBOutlet weak var variableButton: UIButton!
+    
+    @IBOutlet weak var lockButtonText1: UIButton!
+    @IBOutlet weak var lockButtonText2: UIButton!
+    @IBOutlet weak var lockButtonText3: UIButton!
+    
+    @IBOutlet weak var variableButton1: UIButton!
+    @IBOutlet weak var variableButton2: UIButton!
+    @IBOutlet weak var variableButton3: UIButton!
+    
     
     // MARK: Google adwords variable
     @IBOutlet weak var GoogleBannerView: GADBannerView!
-    
-    @IBOutlet weak var variableButtonLock: UIButton!
     
     
     
     //MARK: Clear Button Functionality
     @IBAction func clearButton(_ sender: UIButton) {
+        
+        if variable1IsLocked == false {
+            variableButton1.setTitle("( x )", for: .normal)
+        }
+        
+        if variable2IsLocked == false {
+            variableButton2.setTitle("( y )", for: .normal)
+        }
+        
+        if variable3IsLocked == false {
+            variableButton3.setTitle("( z )", for: .normal)
+        }
+        
         currentDisplay = ""
         mainDisplay.text = currentDisplay
         firstNumber = nil
         secondNumber = nil
+        containsDecimal = false
     }
     
     //MARK: Equals button functionality
@@ -57,36 +83,47 @@ class ViewController: UIViewController {
             secondNumber = Double(mainDisplay.text!)
             
             if operatorType == "+" {
-                answer = String(format: "%.1f", firstNumber! + secondNumber!)
+                answer = firstNumber! + secondNumber!
             }
             
             if operatorType == "-" {
-                answer = String(format: "%.1f", firstNumber! - secondNumber!)
+                answer = firstNumber! - secondNumber!
             }
             
             if operatorType == "/" {
-                answer = String(format: "%.1f", firstNumber! / secondNumber!)
+                answer = firstNumber! / secondNumber!
             }
             
             if operatorType == "x" {
-                answer = String(format: "%.1f", firstNumber! * secondNumber!)
+                answer = firstNumber! * secondNumber!
             }
             
-            
+            string_answer = String(format: "%.2f", answer!)
         
-        currentDisplay = answer
+        currentDisplay = string_answer
         mainDisplay.text = currentDisplay
         UserDefaults.standard.set(mainDisplay.text, forKey: "mainDisplay")
             
             
-        if variableIsLocked == false {
-            variableButton.setTitle(answer, for: .normal)
-            UserDefaults.standard.set(variableButton.currentTitle, forKey: "variableInputButton")
+        if variable1IsLocked == false {
+            variableButton1.setTitle(string_answer, for: .normal)
+            UserDefaults.standard.set(variableButton1.currentTitle, forKey: "variableInputButton")
+        }
+            
+        if variable2IsLocked == false {
+            variableButton2.setTitle(string_answer, for: .normal)
+            UserDefaults.standard.set(variableButton2.currentTitle, forKey: "variable2InputButton")
         }
         
+        if variable3IsLocked == false {
+            variableButton3.setTitle(string_answer, for: .normal)
+            UserDefaults.standard.set(variableButton1.currentTitle, forKey: "variable3InputButton")
+        }
+            
         firstNumber = nil
         secondNumber = nil
         operationIsDisplayed = true
+        containsDecimal = false
             
         }
         
@@ -104,36 +141,38 @@ class ViewController: UIViewController {
             operationIsDisplayed = false
             
         }
-   
-    
+
+            
         // Take the input from the tag on the button pressed, assign it to a readable variable
-        let inputRecieved = sender.tag - 1
+        let inputRecieved:String = String(sender.tag - 1)
         // Create a empty NumberPad Enumeration Variable for the switch statement to assign a value to
-        var numberPressed = Int()
+        var numberPressed = String()
         
         switch inputRecieved {
-        case 0:
-            numberPressed = 0
-        case 1:
-            numberPressed = 1
-        case 2:
-            numberPressed = 2
-        case 3:
-            numberPressed = 3
-        case 4:
-            numberPressed = 4
-        case 5:
-            numberPressed = 5
-        case 6:
-            numberPressed = 6
-        case 7:
-            numberPressed = 7
-        case 8:
-            numberPressed = 8
-        case 9:
-            numberPressed = 9
+        case "0":
+            numberPressed = "0"
+        case "1":
+            numberPressed = "1"
+        case "2":
+            numberPressed = "2"
+        case "3":
+            numberPressed = "3"
+        case "4":
+            numberPressed = "4"
+        case "5":
+            numberPressed = "5"
+        case "6":
+            numberPressed = "6"
+        case "7":
+            numberPressed = "7"
+        case "8":
+            numberPressed = "8"
+        case "9":
+            numberPressed = "9"
+        case "19":
+            numberPressed = "."
         default:
-            numberPressed = 0
+            numberPressed = "cold"
         }
         
         // Clear out the currentDisplay variable if it contains a operator
@@ -141,30 +180,73 @@ class ViewController: UIViewController {
             currentDisplay = ""
         }
         
-        // String the resulting value to send to the mainDisplay label
-        let makeValueIntoString = String(describing: numberPressed)
+        
+        for char in currentDisplay {
+            
+            if char == "." {
+                containsDecimal = true
+            }
+            
+        }
+        
+        if numberPressed == "." && containsDecimal == true {
+            
+            return
+            
+        }
         
         // Append the character the the end of the current string using the mainDisplay global variable
-        currentDisplay.append(makeValueIntoString)
+        currentDisplay.append(numberPressed)
         // Update the mainDisplay's text by accessing and reassigning the global variable to the text object
         mainDisplay.text = currentDisplay
         
     }
+    
+    
  
-    // MARK: Lock/Un-lock Button
+    // MARK: Lock/Un-lock Buttons
     
     @IBAction func updateVariableButtonStatus(_ sender: UIButton) {
         
-        if variableIsLocked == true {
-            variableIsLocked = false
-            variableButtonLock.setTitle("Un-Locked", for: .normal)
+        if variable1IsLocked == true {
+            variable1IsLocked = false
+            lockButtonText1.setTitle("Un-Locked", for: .normal)
+        }
+        else {
+            variable1IsLocked = true
+            lockButtonText1.setTitle("Locked", for: .normal)
         }
         
-        else {
-            variableIsLocked = true
-            variableButtonLock.setTitle("Locked", for: .normal)
-        }
     }
+    
+    @IBAction func updateLockStatus2(_ sender: UIButton) {
+        
+        if variable2IsLocked == true {
+            variable2IsLocked = false
+            lockButtonText2.setTitle("Un-Locked", for: .normal)
+        }
+        else {
+            variable2IsLocked = true
+            lockButtonText2.setTitle("Locked", for: .normal)
+        }
+        
+    }
+    
+    @IBAction func updateLockStatus3(_ sender: Any) {
+        
+        if variable3IsLocked == true {
+            variable3IsLocked = false
+            lockButtonText3.setTitle("Un-Locked", for: .normal)
+        }
+        else {
+            variable3IsLocked = true
+            lockButtonText3.setTitle("Locked", for: .normal)
+        }
+        
+    }
+    
+    
+    
     
     // MARK: Variable Button Input
     
@@ -172,9 +254,9 @@ class ViewController: UIViewController {
         
         if firstNumber == nil {
             
-            if Double(variableButton.currentTitle!) != nil {
+            if Double(variableButton1.currentTitle!) != nil {
             
-                currentDisplay = variableButton.currentTitle!
+                currentDisplay = variableButton1.currentTitle!
                 mainDisplay.text = currentDisplay
             }
             
@@ -182,14 +264,64 @@ class ViewController: UIViewController {
             
         else if secondNumber == nil {
             
-            if Double(variableButton.currentTitle!) != nil {
+            if Double(variableButton1.currentTitle!) != nil {
                 
-                currentDisplay = variableButton.currentTitle!
+                currentDisplay = variableButton1.currentTitle!
                 mainDisplay.text = currentDisplay
             
             }
         }
     }
+    
+    @IBAction func pressedVariableButton2(_ sender: Any) {
+        
+        if firstNumber == nil {
+            
+            if Double(variableButton2.currentTitle!) != nil {
+                
+                currentDisplay = variableButton2.currentTitle!
+                mainDisplay.text = currentDisplay
+            }
+            
+        }
+            
+        else if secondNumber == nil {
+            
+            if Double(variableButton2.currentTitle!) != nil {
+                
+                currentDisplay = variableButton2.currentTitle!
+                mainDisplay.text = currentDisplay
+                
+            }
+        }
+    }
+    
+    @IBAction func pressedVariableButton3(_ sender: Any) {
+        
+        
+        if firstNumber == nil {
+            
+            if Double(variableButton3.currentTitle!) != nil {
+                
+                currentDisplay = variableButton3.currentTitle!
+                mainDisplay.text = currentDisplay
+            }
+            
+        }
+            
+        else if secondNumber == nil {
+            
+            if Double(variableButton3.currentTitle!) != nil {
+                
+                currentDisplay = variableButton3.currentTitle!
+                mainDisplay.text = currentDisplay
+                
+            }
+        }
+        
+    }
+    
+    
     
     //MARK: Operator Button Functionality
  
@@ -219,34 +351,49 @@ class ViewController: UIViewController {
             }
             
             if operatorType == "+" {
-                answer = String(firstNumber! + secondNumber!)
+                answer = firstNumber! + secondNumber!
             }
             
             if operatorType == "-" {
-                answer = String(firstNumber! - secondNumber!)
+                answer = firstNumber! - secondNumber!
             }
             
             if operatorType == "/" {
-                answer = String(firstNumber! / secondNumber!)
+                answer = firstNumber! / secondNumber!
             }
             
             if operatorType == "x" {
-                answer = String(firstNumber! * secondNumber!)
+                answer = firstNumber! * secondNumber!
             }
             
-            // Update the answer box button
-            variableButton.setTitle(answer, for: .normal)
+            string_answer = String(format: "%.2f", answer!)
+            
+            // Update the variable answer buttons if not locked
+            if variable1IsLocked == false {
+                variableButton1.setTitle(string_answer, for: .normal)
+            }
+            
+            if variable2IsLocked == false {
+                variableButton2.setTitle(string_answer, for: .normal)
+            }
+            
+            if variable3IsLocked == false {
+                variableButton3.setTitle(string_answer, for: .normal)
+            }
+  
             
             //Update the current display with the operation the user is performing
             currentDisplay = sender.currentTitle!
             mainDisplay.text = currentDisplay
-            firstNumber = Double(answer)
+            firstNumber = Double(string_answer)
             secondNumber = nil
             operatorType = sender.currentTitle
             
         } else {
             mainDisplay.text = "Please enter a number to start"
         }
+        
+        containsDecimal = false
         
     }
     
@@ -272,7 +419,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         
         if let x = UserDefaults.standard.object(forKey: "variableInputButton") as? String {
-            variableButton.setTitle(x, for: .normal)
+            variableButton1.setTitle(x, for: .normal)
         }
         
         if let y = UserDefaults.standard.object(forKey: "mainDisplay") as? String {
