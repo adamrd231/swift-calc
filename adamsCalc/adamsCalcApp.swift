054101b96192b1e8d3c5539c64f2e76ad8ec22e7
adamrd231 <adam.rdconcepts@gmail.com>
2021-06-17 12:01:08 -0600

    Initial Commit

---
+++ adamsCalc/adamsCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  adamsCalcApp.swift
+//  adamsCalc
+//
+//  Created by Adam Reed on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct adamsCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
